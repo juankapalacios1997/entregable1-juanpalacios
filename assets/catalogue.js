d3764c8bf76acd174cae56e05ab99eb4b2822bba
juankapalacios1997 <jcmaster_1997@hotmail.com>
2025-02-06 14:19:39 -0600

    Anadiendo las imagenes

--- assets/catalogue.js
+++ assets/catalogue.js
@@ -1,74 +1,86 @@
 export const catalogoCompleto = [
     {
-        nombre: "Johnny Walker Black Label",
-        categoria: "Whisky",
-        cantidad: " 750 ml",
-        precioUsd: 51.99
-    },
-    {
-        nombre: "Johnny Walker Blue Label",
-        categoria: "Whisky",
-        cantidad: " 750 ml",
-        precioUsd: 109.99
-    },
-    {
-        nombre: "Buchanan's Deluxe 12 anos Blended Scotch",
-        categoria: "Whisky",
-        cantidad: " 750 ml",
-        precioUsd: 21.99
-    },
-    {
-        nombre: "Chivas Regal 12 anos",
-        categoria: "Whisky",
-        cantidad: "750 ml",
-        precioUsd: 65.99
-    },
-    {
-        nombre: "Ballantines",
-        categoria: "Whisky",
-        cantidad: "700 ml",
-        precioUsd: 19.99
-    },
-    {
         nombre: "Don Julio Reposado",
         categoria: "Tequila",
         cantidad: " 700 ml",
-        precioUsd: 51.99
+        precioUsd: 51.99,
+        imagen: "./assets/images/don-julio-reposado.jpg"
     },
     {
         nombre: "Gran Centenario",
         categoria: "Tequila",
         cantidad: " 950 ml",
-        precioUsd: 15.99
+        precioUsd: 15.99,
+        imagen: "./assets/images/gran-centenario.jpg"
     },
     {
         nombre: "Mestro Dobel Diamante",
         categoria: "Tequila",
         cantidad: " 700 ml",
-        precioUsd: 71.99
+        precioUsd: 71.99,
+        imagen: "./assets/images/maestro-dobel-diamante.jpg"
     },
     {
         nombre: "Jose Cuervo Tradicional Reposado",
         categoria: "Tequila",
         cantidad: " 700 ml",
-        precioUsd: 34.99
+        precioUsd: 34.99,
+        imagen: "./assets/images/jose-cuervo-tradicional-reposado.png"
     },
     {
         nombre: "Smirnoff X1 Tamarindo",
         categoria: "Vodka",
         cantidad: "750ml",
-        precioUsd: 9.99
+        precioUsd: 9.99,
+        imagen: "./assets/images/smirnoff-x1-tamarindo.png"
     },
     {
         nombre: "Stolichnaya Spirit",
         categoria: "Vodka",
         cantidad: "750ml",
-        precioUsd: 14.99
+        precioUsd: 14.99,
+        imagen: "./assets/images/stolichnaya-spirit.jpg"
     },
     {
         nombre: "Absolut Azul 750ml",
         categoria: "Vodka",
         cantidad: "700ml",
-        precioUsd: 19.99
+        precioUsd: 19.99,
+        imagen: "./assets/images/absolut-azul.jpg"
+    },
+    {
+        nombre: "Buchanan's Deluxe 12 anos Blended Scotch",
+        categoria: "Whisky",
+        cantidad: " 750 ml",
+        precioUsd: 21.99,
+        imagen: "./assets/images/buchanans-deluxe-12-anos.jpg"
+    },
+    {
+        nombre: "Chivas Regal 12 anos",
+        categoria: "Whisky",
+        cantidad: "750 ml",
+        precioUsd: 65.99,
+        imagen: "./assets/images/chivas-regal-12-anos.jpg"
+    },
+    {
+        nombre: "Ballantines",
+        categoria: "Whisky",
+        cantidad: "700 ml",
+        precioUsd: 19.99,
+        imagen: "./assets/images/ballantines.jpg"
+    },
+    {
+        nombre: "Johnny Walker Black Label",
+        categoria: "Whisky",
+        cantidad: " 750 ml",
+        precioUsd: 51.99,
+        imagen: "./assets/images/johnny-walker-black-label.jpg"
+    },
+    {
+        nombre: "Johnny Walker Blue Label",
+        categoria: "Whisky",
+        cantidad: " 750 ml",
+        precioUsd: 109.99,
+        imagen: "./assets/images/coco-basile.jpg"
     },
 ]
