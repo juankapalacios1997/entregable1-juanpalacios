c175801c64959afb9c7c6573ec523f42ee684a32
juankapalacios1997 <jcmaster_1997@hotmail.com>
2025-02-06 13:13:43 -0600

    Creating card components

--- js/main.js
+++ js/main.js
@@ -1,26 +1,41 @@
 import { catalogoCompleto } from "../assets/catalogue.js";
 
-console.log(catalogoCompleto);
+function bienvenida() {
+    const nombre = prompt("Bienvenido a la vinateria 'La Gran Bodega'. Por favor introduce tu nombre:");
+
+    if (!nombre) {
+        alert(`Ocurrio un error, por favor intente mas tarde`);
+    }
+
+    return nombre;
+}
+
+function conversionDolarPesoArg(precio) {
+    const dolarPeso = 1041.55;
+
+    const conversion = precio * dolarPeso;
+
+    return `$${conversion.toFixed(2)}`;
+}
+
 const cardContainer = document.getElementById("cardContainer");
 
-console.log(cardContainer);
-
-catalogoCompleto.forEach((item) => {
-    const card = document.createElement("span");
-    card.className = "card-item"
-    card.innerText = JSON.stringify(item);
-    console.log(card, item);
-    cardContainer.appendChild(card);
-})
-
-// function bienvenida() {
-//     const nombre = prompt("Bienvenido a la vinateria 'La Gran Bodega'. Por favor introduce tu nombre:");
-
-//     if (!nombre) {
-//         alert(`Ocurrio un error, por favor intente mas tarde`);
-//     }
-
-//     return nombre;
-// }
+// catalogoCompleto.forEach((item) => {
+//     const card = document.createElement("span");
+//     card.className = "card-item"
+//     card.innerHTML = `
+//         <div>
+//         <p>${item.nombre}</p>
+//         <p>${item.categoria}</p>
+//         <p>${conversionDolarPesoArg(item.precioUsd)}</p>
+//         </div>
+//     `;
+//     console.log(card, item);
+//     cardContainer.appendChild(card);
+// })
 
 // const nombre = bienvenida();
+
+// const mainTitle = document.getElementById("mainTitle");
+
+// mainTitle.innerText = `Bienvenido a la pagina de la vinateria "La Gran Bodega", Sr(a) ${nombre}.`;
